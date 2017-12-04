116dc815aa065ff073fbabcd1f9b3d1dc940323b
Ruslan Gainutdinov <ruslanfg@gmail.com>
2017-12-04 12:51:17 +0300

    More escapes

--- gitlog.sh
+++ gitlog.sh
@@ -6,7 +6,7 @@ revlist=$(git rev-list HEAD)
   echo '<log>'
   for rev in $revlist
   do
-    echo "$(git log -1 --date=iso --pretty=format:"<logentry revision=\"%h\">%n<author>%an</author>%n<date>%ad</date>%n<msg>%B</msg>%n" $rev | sed -re "s/&/&amp;/g")"
+    echo "$(git log -1 --date=iso --pretty=format:"<logentry revision=\"%h\">%n<author>%an</author>%n<date>%ad</date>%n<msg><![CDATA[%B]]></msg>%n" $rev | sed -re "s/&/&amp;/g;s/\[\]//g")"
     files=$(git log -1 --pretty="format:" --name-only $rev)
     echo '<paths>'
     for file in $files
